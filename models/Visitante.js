2054772e0027a85fc21b7dcf49ed5c02f52475b5
Camalu Atacado <camaluatacado@gmail.com>
2025-02-11 13:06:11 -0300

    updt

--- models/Visitante.js
+++ models/Visitante.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
-const VisitanteSchema = new mongoose.Schema({
-  revendedorToken: { type: String, required: true },
-  idFatura: { type: String, default: null },
-  nome: { type: String, required: true },
-  renavam: { type: String, required: true },
-  estado: { type: String, required: true },
-  adquirente: { type: String, default: null },
-  ip: String,
-  userAgent: String,
-  navegador: String,
-  dispositivo: String,
-  valorGerado: { type: Number, default: 0 },
-  parcelasSelecionadas: { type: [Number], default: [] },
-  statusPagamento: {
-    type: String,
-    enum: ["não gerado", "gerado", "pago"],
-    default: "não gerado", // 🔥 Opção padrão adicionada
+const VisitanteSchema = new mongoose.Schema(
+  {
+    revendedorToken: { type: String, required: true },
+    idFatura: { type: String, default: null },
+    nome: { type: String, required: true },
+    renavam: { type: String, required: true },
+    estado: { type: String, required: true },
+    adquirente: { type: String, default: null },
+    ip: String,
+    userAgent: String,
+    navegador: String,
+    dispositivo: String,
+    valorGerado: { type: Number, default: 0 },
+    parcelasSelecionadas: { type: [Number], default: [] },
+    statusPagamento: {
+      type: String,
+      enum: ["não gerado", "gerado", "pago"],
+      default: "não gerado", // 🔥 Opção padrão adicionada
+    },
+    dataAcesso: { type: Date, default: Date.now },
   },
-  dataAcesso: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 const Visitante = mongoose.model("MKVisitante", VisitanteSchema);
 export default Visitante;
